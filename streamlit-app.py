126d630ea67bbaab96fced2da901c116f5009759
mmalonehallberg-analytics8 <125911914+mmalonehallberg-analytics8@users.noreply.github.com>
2023-02-20 13:34:13 -0500

    Update streamlit-app.py
    menu updated per course

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Blueberry Muffin')
-streamlit.text('Eggwhite Omelette')
-streamlit.text('Berry Parfait')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
