df864f1769bbe9df8a58ff5b9f3158d6224f3eea
mmalonehallberg-analytics8 <125911914+mmalonehallberg-analytics8@users.noreply.github.com>
2023-02-20 13:33:09 -0500

    Update streamlit-app.py
    added menu text

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry Muffin')
+streamlit.text('Eggwhite Omelette')
+streamlit.text('Berry Parfait')
