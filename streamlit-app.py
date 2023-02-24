f3ec1cf89f14c256a23853bdae3d85e0487f71b9
mmalonehallberg-analytics8 <125911914+mmalonehallberg-analytics8@users.noreply.github.com>
2023-02-24 18:39:55 -0500

    Update streamlit-app.py
    correct fruit choice variable

--- streamlit-app.py
+++ streamlit-app.py
@@ -44,4 +44,4 @@ streamlit.dataframe(my_data_rows)
 
 # Allow user to add fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+streamlit.write('Thanks for adding ', add_my_fruit)
